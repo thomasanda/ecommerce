408a04cd54bcacf9880feaef173aaf94da67f890
thomasanda <{ID}+{username}@users.noreply.github.com>
2022-02-14 19:05:07 +0100

    added routing collection preview and beginning of shop page

---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import SHOP_DATA from './shop.data'
+import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+
+const ShopPage = () => {
+  return <div className="shop-page">
+    {
+      collections.map
+    }
+
+  </div>
+}
+
+export default ShopPage;
