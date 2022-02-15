2b7f9c8169fcd8258137e0e6b1c1074a27beaeb9
thomasanda <{ID}+{username}@users.noreply.github.com>
2022-02-15 16:14:21 +0100

    added collection preview, shop component, collection-items component and header all with styling

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 
-import SHOP_DATA from './shop.data'
+import SHOP_DATA from './shop.data';
 import CollectionPreview from '../../components/collection-preview/collection-preview.component';
 
 const ShopPage = () => {
-  return <div className="shop-page">
+  console.log(SHOP_DATA)
+  return (<div className="shop-page">
     {
-      collections.map
+      SHOP_DATA.map(({ id, ...otherCollectionProps }) => (<CollectionPreview key={id} {...otherCollectionProps} />
+      ))
     }
-
   </div>
+  );
 }
 
 export default ShopPage;
